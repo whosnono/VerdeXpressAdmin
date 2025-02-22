bd98aede3dba98babcd0c33c9199c483ff8d642c
eli <ebmv03@gmail.com>
2025-02-21 21:28:10 -0700

    añade segunda versión del navbar

--- core/navigation/src/main/java/com/example/navigation/AppNavHost.kt
+++ core/navigation/src/main/java/com/example/navigation/AppNavHost.kt
@@ -1,6 +1,7 @@
 package com.example.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,8 +12,8 @@ import com.example.home.HomeScreen
 //#todo: import com.example.profile.ProfileScreen
 
 @Composable
-fun AppNavHost(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
+fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(navController = navController, startDestination = NavigationItem.Home.route, modifier = modifier) {
         composable(NavigationItem.Home.route) { HomeScreen() }
         //#todo:añadir composable(NavigationItem.Parks.route) { ParksScreen() }
         //#todo:añadir composable(NavigationItem.Donations.route) { DonationsScreen() }
--- feature/home/src/main/java/com/example/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/HomeScreen.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-    Text("Home Screen")
+    Text("Home Screen!!!!")
 }
