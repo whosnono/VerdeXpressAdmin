ebf895cc7f47f19bd3143b979425c448c4ab7712
eli <ebmv03@gmail.com>
2025-02-24 20:25:07 -0700

    primera versión de ResetPasswordScreen

--- feature/auth/src/main/java/com/example/auth/ui/ResetPasswordScreen.kt
+++ feature/auth/src/main/java/com/example/auth/ui/ResetPasswordScreen.kt
@@ -1,2 +1,120 @@
 package com.example.auth.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.design.R
+import com.example.design.SecondaryAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ResetPasswordScreen(navController: NavController) {
+    val verdeBoton = Color(0xFF78B153)
+    val roundedShape = RoundedCornerShape(12.dp)
+
+    Column {
+        SecondaryAppBar(
+            showIcon = true,
+            onIconClick = {
+                navController.navigate("Home") // #TODO: Esto debe redireccionar a SignIn
+            }
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "VerdeXpress",
+                fontSize = 24.sp,
+                color = verdeBoton,
+                fontFamily = FontFamily(Font(R.font.sf_pro_display_bold)),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Text(
+                text = "Restablecer contraseña",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(Font(R.font.sf_pro_display_bold)),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = "Se enviarán las instrucciones para restablecer su contraseña a su correo electrónico.",
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            var correoElectronico by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = correoElectronico,
+                onValueChange = { correoElectronico = it },
+                label = {
+                    Text(
+                        text = "Correo electrónico",
+                        color = Color.Gray,
+                        fontFamily = FontFamily(Font(R.font.sf_pro_display_regular))
+                    )
+                },
+                shape = roundedShape,
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = verdeBoton,
+                    focusedLabelColor = verdeBoton,
+                    cursorColor = verdeBoton
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { /* Acción al hacer clic */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = verdeBoton),
+                shape = roundedShape
+            ) {
+                Text(
+                    text = "Enviar",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.sf_pro_display_medium))
+                )
+            }
+        }
+    }
+}
