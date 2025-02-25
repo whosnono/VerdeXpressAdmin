98d6567a34e26ebb996025d960d23580790e026a
arcanefairy <130689169+arcanefairy@users.noreply.github.com>
2025-02-24 20:27:11 -0700

    Página de inicio de sesión+logo

--- feature/auth/src/main/java/com/example/auth/ui/SignInScreen.kt
+++ feature/auth/src/main/java/com/example/auth/ui/SignInScreen.kt
@@ -1,2 +1,129 @@
 package com.example.auth.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.auth.R
+
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(18.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(bottom = 19.dp)
+        )
+
+        Text(
+            text = "VerdeXpress",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF78B153)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+        ) {
+            Text(
+                text = "Iniciar sesión",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        var usuario by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = usuario,
+            onValueChange = { usuario = it },
+            label = { Text(text = "Usuario") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        var password by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Contraseña") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(3.dp))
+
+        TextButton(onClick = {  }) {
+            Text(text = "Recuperar contraseña", color = Color(0xFF78B153))
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {  },
+            modifier = Modifier.widthIn(max = 250.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF78B153))
+        ) {
+            Text("Ingresar", color = Color.White, fontSize = 20.sp)
+        }
+
+        Spacer(modifier = Modifier.height(1.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "¿No tienes cuenta?", fontSize = 15.sp)
+            TextButton(onClick = {  }) {
+                Text(text = "Regístrate", color = Color(0xFF78B153), fontSize = 14.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VerLogin() {
+    Login()
+}
