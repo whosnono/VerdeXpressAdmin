4fea22d7cccf715f294dd30b9cfbced9dad7ddb1
eli <ebmv03@gmail.com>
2025-03-16 12:48:33 -0700

    agrega funcionalidad para guardar donaciones en Firestore y agrega gestión de imágenes en Supabase

--- feature/donations/src/main/java/com/example/donations/ui/donacionEspecie/FormScreen.kt
+++ feature/donations/src/main/java/com/example/donations/ui/donacionEspecie/FormScreen.kt
@@ -4,20 +4,37 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -32,24 +49,28 @@ import com.example.design.R
 import com.example.design.SecondaryAppBar
 import com.example.donations.data.GetParkNameAndLocation
 import com.example.donations.data.ParkData
+import com.example.donations.data.donacionEspecie.saveDonationToFirestore
 import com.example.donations.ui.donacionEspecie.reu.CustomDropdown
 import com.example.donations.ui.donacionEspecie.reu.CustomOutlinedTextField
+import com.example.parks.data.ImageUploadManager
 import com.example.parks.ui.ParkImageDisplay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun FormScreen(navController: NavController) {
 
     val verdeBoton = Color(0xFF78B153)
     val roundedShape = RoundedCornerShape(12.dp)
-    // var showSuccessDialog by remember { mutableStateOf(false) } //dialogo de exito
-    // var showFailedDialog by remember { mutableStateOf(false) } //dialogo de error
+    var showSuccessDialog by remember { mutableStateOf(false) } //dialogo de exito
+    var showFailedDialog by remember { mutableStateOf(false) } //dialogo de error
 
     var name by rememberSaveable { mutableStateOf("") }
     var contactNumber by remember { mutableStateOf("") }
     var location by rememberSaveable { mutableStateOf("") }
     var numeroValue by remember { mutableStateOf("") }
     var selectedImageUris by remember { mutableStateOf<List<Uri>>(emptyList()) }
-    // Estado para la opción seleccionada
     var selectedOption by remember { mutableStateOf("") }
     var imageError by remember { mutableStateOf<String?>(null) }
     var nameError by remember { mutableStateOf<String?>(null) }
@@ -66,10 +87,25 @@ fun FormScreen(navController: NavController) {
     var optionsPark by remember { mutableStateOf<List<String>>(emptyList()) }
     var parksList by remember { mutableStateOf<List<ParkData>>(emptyList()) }
     val optionsType = remember { listOf("Mobiliario", "Jardinería") }
-    val optionsResourceM = remember { listOf("Bancas", "Mesas", "Juegos Infantiles", "Cestos de basura", "Cercas o Delimitaciones") }
-    val optionsResourceJ = remember { listOf("Árboles", "Arbustos", "Plantas Ornamentales", "Césped o Pasto en rollo", "Tierra para rehabilitación de suelos" ) }
+    val optionsResourceM = remember {
+        listOf(
+            "Bancas", "Mesas", "Juegos Infantiles", "Cestos de basura", "Cercas o Delimitaciones"
+        )
+    }
+    val optionsResourceJ = remember {
+        listOf(
+            "Árboles",
+            "Arbustos",
+            "Plantas Ornamentales",
+            "Césped o Pasto en rollo",
+            "Tierra para rehabilitación de suelos"
+        )
+    }
     val optionsCondition = remember { listOf("Nuevo", "Semi-nuevo", "Usado") }
     val context = LocalContext.current
+    val imageUploadManager = remember { ImageUploadManager(context) }
+    var isUploading by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
     var showPermissionDialog by remember { mutableStateOf(false) }
     val resourceOptions = remember(selectedResourceType) {
         when (selectedResourceType) {
@@ -80,23 +116,17 @@ fun FormScreen(navController: NavController) {
     }
     val validator = rememberDonationFormValidator()
 
-    // Instancia del objeto para obtener los parques
     val getParkNameAndLocation = GetParkNameAndLocation()
 
-    // Obtener los parques
     LaunchedEffect(Unit) {
-        getParkNameAndLocation.getParkNameAndLocation(
-            onSuccess = { parks ->
-                parksList = parks
-                optionsPark = parks.map { it.nombre }
-            },
-            onFailure = { exception ->
-                println("Error al obtener parques")
-            }
-        )
+        getParkNameAndLocation.getParkNameAndLocation(onSuccess = { parks ->
+            parksList = parks
+            optionsPark = parks.map { it.nombre }
+        }, onFailure = { exception ->
+            println("Error al obtener parques")
+        })
     }
 
-    // Lanzador para seleccionar imágenes
     val imagePickerLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.GetMultipleContents()
     ) { uris ->
@@ -112,7 +142,6 @@ fun FormScreen(navController: NavController) {
         }
     }
 
-    // Función para abrir el selector de imágenes
     fun openImagePicker() {
         if (selectedImageUris.size >= 3) {
             imageError = "Solo puedes subir un máximo de 3 imágenes."
@@ -121,19 +150,16 @@ fun FormScreen(navController: NavController) {
         }
     }
 
-    // Lanzador para solicitar permisos
     val permissionLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
             openImagePicker()
         } else {
-            // Mostrar un mensaje al usuario indicando que el permiso es necesario
             showPermissionDialog = true
         }
     }
 
-    // Función para verificar y solicitar permisos
     fun checkAndRequestPermission() {
         val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             Manifest.permission.READ_MEDIA_IMAGES
@@ -141,15 +167,19 @@ fun FormScreen(navController: NavController) {
             Manifest.permission.READ_EXTERNAL_STORAGE
         }
 
-        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                context, permission
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             openImagePicker()
         } else {
             permissionLauncher.launch(permission)
         }
     }
+
     val scrollState = rememberScrollState()
 
-    fun validate(){
+    fun validate() {
         val formState = DonationFormState(
             donorName = name,
             contactNumber = contactNumber,
@@ -173,19 +203,42 @@ fun FormScreen(navController: NavController) {
         statusErrorN = validationResult.quantityError
         imageError = validationResult.imageError
 
-        /*//descomentar para probar dialogos
-        if (nameError == null && contactNumberError == null && locationError == null &&
-            statusErrorP == null && statusErrorT == null && statusErrorR == null &&
-            statusErrorC == null && statusErrorN == null && imageError == null) {
-            showSuccessDialog = true //descomentar para probar dialogo de exito
-            // showFailedDialog = true //descomentar para probar dialogo de error
-        }*/
+        if (validationResult.isValid()) {
+            isUploading = true // Activar el estado de subida
+            coroutineScope.launch {
+                try {
+                    val imageUrls = imageUploadManager.uploadImages(selectedImageUris)
 
+                    saveDonationToFirestore(
+                        name = name,
+                        contactNumber = contactNumber,
+                        location = location,
+                        parkToDonate = selectedOption,
+                        resourceType = selectedResourceType,
+                        resource = selectedResource,
+                        quantity = numeroValue,
+                        condition = selectedCondition,
+                        imageUrls = imageUrls
+                    )
+
+                    withContext(Dispatchers.Main) {
+                        isUploading = false // Desactivar el estado de subida
+                        showSuccessDialog = true // Mostrar diálogo de éxito
+                        navController.navigate("donationSuccess") // Navegar a la pantalla de éxito
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        isUploading = false // Desactivar el estado de subida en caso de error
+                        showFailedDialog = true // Mostrar diálogo de error
+                        Log.e("FormScreen", "Error: ${e.message}", e)
+                    }
+                }
+            }
+        }
     }
 
     Box {
         Column {
-            //AppBar
             SecondaryAppBar(showIcon = true, onIconClick = {
                 navController.popBackStack("Donaciones", inclusive = false)
                 navController.navigate("Donaciones")
@@ -226,7 +279,6 @@ fun FormScreen(navController: NavController) {
                         if (newValue.all { it.isDigit() } && newValue.length <= 10) {
                             contactNumber = newValue
                         }
-                        // Validación del número de contacto
                         contactNumberError = when {
                             newValue.isEmpty() -> "El número de contacto no puede estar vacío"
                             newValue.length < 10 -> "El número de contacto debe tener al menos 10 dígitos"
@@ -237,7 +289,7 @@ fun FormScreen(navController: NavController) {
                     isError = contactNumberError != null,
                     errorMessage = contactNumberError,
                     modifier = Modifier.fillMaxWidth(),
-                    isNumberField = true // Indicar que es un campo numérico
+                    isNumberField = true
                 )
 
                 Spacer(modifier = Modifier.height(10.dp))
@@ -250,7 +302,6 @@ fun FormScreen(navController: NavController) {
                         selectedOption = option
                         statusErrorP = if (option.isEmpty()) "Debes seleccioanr un parque" else null
 
-                        // Buscar la ubicación correspondiente al parque seleccionado
                         val selectedPark = parksList.find { it.nombre == option }
                         location = selectedPark?.ubicacion ?: "Desconocido"
                     },
@@ -308,7 +359,7 @@ fun FormScreen(navController: NavController) {
                     CustomOutlinedTextField(
                         value = numeroValue,
                         onValueChange = { newValue ->
-                            numeroValue = newValue // Actualiza el estado en el padre
+                            numeroValue = newValue
                             val cantidad = newValue.toIntOrNull() ?: 1
                             statusErrorN = when {
                                 cantidad < 1 -> "La cantidad mínima es 1"
@@ -367,9 +418,7 @@ fun FormScreen(navController: NavController) {
                     }
                 }
 
-                // Mostrar las imágenes seleccionadas con scroll mejorado y indicadores visuales
-                ParkImageDisplay(
-                    imageUris = selectedImageUris,
+                ParkImageDisplay(imageUris = selectedImageUris,
                     onImageRemove = { index -> removeImage(index) })
 
                 Spacer(modifier = Modifier.height(30.dp))
@@ -381,37 +430,42 @@ fun FormScreen(navController: NavController) {
                         .height(50.dp)
                         .padding(horizontal = 118.dp),
                     colors = ButtonDefaults.buttonColors(containerColor = verdeBoton),
-                    shape = roundedShape
+                    shape = roundedShape,
+                    enabled = !isUploading
                 ) {
-                    Text(
-                        text = "Validar",
-                        fontSize = 16.sp,
-                        fontFamily = FontFamily(Font(R.font.sf_pro_display_bold))
-                    )
+                    if (isUploading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(24.dp), color = Color.White, strokeWidth = 2.dp
+                        )
+                    } else {
+                        Text(
+                            text = "Validar",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.sf_pro_display_bold))
+                        )
+                    }
                 }
             }
         }
 
-        /*//Borrar si ya no se ocupa
-        SuccessDialog(
-            visible = showSuccessDialog,
-            onDismiss = {
-                showSuccessDialog = false
-                // Navegar de vuelta a la pantalla de inicio
-                navController.popBackStack("Donaciones", inclusive = false)
-                navController.navigate("Donaciones")
-            }
-        )*/
+        // Fondo oscuro semitransparente cuando se muestra un diálogo
+        if (showSuccessDialog || showFailedDialog) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.5f))) // Fondo oscuro semitransparente
+        }
 
-        /* //Borrar si ya no se ocupa
-        FailedDialog(
-            visible = showFailedDialog,
-            onDismiss = {
-                showFailedDialog = false
-                // Navegar de vuelta a la pantalla de inicio
-                navController.popBackStack("Donaciones", inclusive = false)
-                navController.navigate("Donaciones")
-            }
-        )*/
+        SuccessDialog(visible = showSuccessDialog, onDismiss = {
+            showSuccessDialog = false
+            navController.popBackStack("Donaciones", inclusive = false)
+            navController.navigate("Donaciones")
+        })
+
+        FailedDialog(visible = showFailedDialog, onDismiss = {
+            showFailedDialog = false
+            navController.popBackStack("Donaciones", inclusive = false)
+            navController.navigate("Donaciones")
+        })
     }
 }
