73c2598ba450a7def4a6d397e5fefddafae570e1
eli <ebmv03@gmail.com>
2025-03-16 12:48:33 -0700

    agrega funcionalidad para guardar donaciones en Firestore y agrega gestión de imágenes en Supabase

--- feature/donations/build.gradle.kts
+++ feature/donations/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.ui.tooling.preview.android)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
